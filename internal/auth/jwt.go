348c71381505f15272be124a283ee8793b95a954
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-02-26 17:21:20 +0200

    generate jwt tokens for logged in user

---
+++ internal/auth/jwt.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+    "time"
+    "github.com/dgrijalva/jwt-go"
+    "hextech_interview_project/config"
+)
+
+// GenerateJWT creates a new token for the user
+func GenerateJWT(userID int) (string, error) {
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "user_id": userID,
+        "exp":     time.Now().Add(time.Hour * 24).Unix(), // Token valid for 24 hours
+    })
+
+    return token.SignedString(config.JwtSecret)
+}
