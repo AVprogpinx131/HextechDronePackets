55e9f5363131c6dc2ef8b7bebb738b26c3df1841
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-02-26 17:26:49 +0200

    protect endpoints

---
+++ internal/api/protected.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+    "encoding/json"
+    "hextech_interview_project/internal/auth"
+    "net/http"
+)
+
+// Protected handler that requires authentication
+func ProtectedHandler(w http.ResponseWriter, r *http.Request) {
+    userID, err := auth.GetUserID(r)
+    if err != nil {
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+        return
+    }
+
+    json.NewEncoder(w).Encode(map[string]interface{}{
+        "message": "You accessed a protected route!",
+        "user_id": userID,
+    })
+}
