da03bb4e3bf35f640dfb9df9b23f8e1a370b318d
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-02-26 13:35:15 +0200

    verifying if the drone packet was not inserted to the database

--- internal/repository/drone_repository.go
+++ internal/repository/drone_repository.go
@@ -4,30 +4,21 @@ import (
     "database/sql"
     _ "github.com/lib/pq"
     "log"
-    "os"
     "hextech_interview_project/internal/models"
+    "hextech_interview_project/config"
     "fmt"
-    "github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
 
 func InitDB() {
-    // Load .env file
-    err := godotenv.Load("config/.env")
-    if err != nil {
-        log.Println("Warning: No .env file found")
-    }
-
-    // Read DATABASE_URL
-    dbURL := os.Getenv("DATABASE_URL")
-    if dbURL == "" {
-        log.Fatal("DATABASE_URL is not set in .env file")
-    }
+    // Load configuration
+    co.LoadConfig()
 
     // Connect to PostgreSQL
-    db, err = sql.Open("postgres", dbURL)
+    var err error
+    db, err = sql.Open("postgres", co.DbURL)
     if err != nil {
         log.Fatal("Failed to connect to database:", err)
     }
@@ -44,5 +35,8 @@ func InitDB() {
 func SavePacket(packet models.DronePacket) error {
 	query := `INSERT INTO drone_packets (mac, lat, lon, altitude) VALUES ($1, $2, $3, $4)`
 	_, err := db.Exec(query, packet.MAC, packet.Lat, packet.Lon, packet.Altitude)
-	return err
+	if err != nil {
+        log.Println("Error inserting packet:", err)
+    }
+    return err
 }
