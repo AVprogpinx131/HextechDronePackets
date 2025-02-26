90bdd982fd75e0c1c2dbb3e70747bf3b6870aa6a
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-02-26 13:30:38 +0200

    added more logging for simplicity

--- internal/websocket/handler.go
+++ internal/websocket/handler.go
@@ -11,8 +11,19 @@ func ProcessPacket(data []byte) {
     var packet models.DronePacket
     err := json.Unmarshal(data, &packet)
     if err != nil {
-        log.Println("Invalid packet:", err)
+        log.Println("Invalid packet format:", err)
         return
     }
-    repository.SavePacket(packet) // Save packet in the database
+
+    log.Println("Received packet:", packet)
+
+    // Save packet to the database
+    if err := repository.SavePacket(packet); err != nil {
+        log.Println("Error saving packet:", err)
+    } else {
+        log.Println("Packet saved successfully:", packet)
+    }
+
+    // Notify users about the new drone event
+    NotifyUsers("Drone detected: " + packet.MAC)
 }
