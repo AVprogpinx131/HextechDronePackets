5b4287798c565889456f4f6363e5099a1b3bd7cf
Andrus Vaher <93032794+AVprogpinx131@users.noreply.github.com>
2025-02-28 16:13:15 +0200

    Update drone.go

--- internal/models/drone.go
+++ internal/models/drone.go
@@ -2,7 +2,7 @@ package models
 
 type DronePacket struct {
     MAC      string  `json:"mac"`
-    Latitude      float64 `json:"lat"`
-    Longitude      float64 `json:"lon"`
+    Latitude      float64 `json:"latitude"`
+    Longitude      float64 `json:"longitude"`
     Altitude float64 `json:"altitude"`
 }
