a6a1481c2749be113bf37bf834f34575d44ea93b
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-02-28 16:09:24 +0200

    get all territories for all users, fixed delete territory functionality

--- internal/repository/territory.go
+++ internal/repository/territory.go
@@ -3,6 +3,8 @@ package repository
 import (
     "hextech_interview_project/internal/models"
     "log"
+    "errors"
+    "database/sql"
 )
 
 // Create a new territory
@@ -38,14 +40,44 @@ func GetTerritories(userID int) ([]models.Territory, error) {
     return territories, nil
 }
 
+
+// Retrieves all territories in the database
+func GetAllTerritories() ([]models.Territory, error) {
+    query := `SELECT * FROM territories`
+    rows, err := db.Query(query)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    var territories []models.Territory
+    for rows.Next() {
+        var t models.Territory
+        err := rows.Scan(&t.ID, &t.UserID, &t.Name, &t.Latitude, &t.Longitude, &t.Radius)
+        if err != nil {
+            return nil, err
+        }
+        territories = append(territories, t)
+    }
+    return territories, nil
+}
+
+
 // Delete a territory by ID
 func DeleteTerritory(userID, territoryID int) error {
-    query := `DELETE FROM territories WHERE id = $1 AND user_id = $2`
-    _, err := db.Exec(query, territoryID, userID)
+    query := `DELETE FROM territories WHERE id = $1 AND user_id = $2 RETURNING id`
+    var deletedID int
+    err := db.QueryRow(query, territoryID, userID).Scan(&deletedID)
+
     if err != nil {
-        log.Println("Error deleting territory:", err)
-    } else {
-        log.Println("Territory deleted:", territoryID)
+        if err == sql.ErrNoRows {
+            log.Printf("No territory found with ID: %d for user: %d", territoryID, userID)
+            return errors.New("territory not found")
+        }
+        log.Println("❌ Error deleting territory:", err)
+        return err
     }
-    return err
+
+    log.Printf("Territory deleted: %d", deletedID)
+    return nil
 }
