23e20a0afe2fc31541dc4a16ec2a8bb4e8adb8c4
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-03-01 18:09:12 +0200

    added drone movement struct

--- internal/models/drone.go
+++ internal/models/drone.go
@@ -6,3 +6,12 @@ type DronePacket struct {
     Longitude      float64 `json:"longitude"`
     Altitude float64 `json:"altitude"`
 }
+
+type DroneMovement struct {
+    ID           int     `json:"id"`
+    MAC          string  `json:"mac"`
+    EventType    string  `json:"event_type"`
+    TerritoryId  int     `json:"territory_id"`
+    TerritoryName string `json:"territory_name"`
+    Timestamp    string  `json:"timestamp"`
+}
