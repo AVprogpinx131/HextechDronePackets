de210d8b85c525f593a0430bc2e21628eae9dece
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-03-05 14:29:40 +0200

    added drone in territory struct

--- internal/models/drone.go
+++ internal/models/drone.go
@@ -1,19 +1,27 @@
 package models
 
 type DronePacket struct {
-    MAC      string  `json:"mac"`
-    Latitude      float64 `json:"latitude"`
-    Longitude      float64 `json:"longitude"`
-    Altitude float64 `json:"altitude"`
+    MAC         string  `json:"mac"`
+    Latitude    float64 `json:"latitude"`
+    Longitude   float64 `json:"longitude"`
+    Altitude    float64 `json:"altitude"`
+}
+
+type DroneInTerritory struct {
+    MAC             string  `json:"mac"`
+    Latitude        float64 `json:"latitude"`
+    Longitude       float64 `json:"longitude"`
+    Altitude        float64 `json:"altitude"`
+    TerritoryName   string `json:"territory_name"`
 }
 
 type DroneMovement struct {
-    ID           int     `json:"id"`
-    MAC          string  `json:"mac"`
-    EventType    string  `json:"event_type"`
-    TerritoryId  int     `json:"territory_id"`
-    TerritoryName string `json:"territory_name"`
-    Timestamp    string  `json:"timestamp"`
-    MinAltitude  float64 `json:"min_altitude"`
-    MaxAltitude  float64 `json:"max_altitude"`
+    ID              int     `json:"id"`
+    MAC             string  `json:"mac"`
+    EventType       string  `json:"event_type"`
+    TerritoryId     int     `json:"territory_id"`
+    TerritoryName   string `json:"territory_name"`
+    Timestamp       string  `json:"timestamp"`
+    MinAltitude     float64 `json:"min_altitude"`
+    MaxAltitude     float64 `json:"max_altitude"`
 }
