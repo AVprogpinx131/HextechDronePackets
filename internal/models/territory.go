b16789d91d2d6c8e864016ec10c03d77df2f1afe
AVprogpinx131 <andrusvaher2004@gmail.com>
2025-03-05 14:30:09 +0200

    align struct fields

--- internal/models/territory.go
+++ internal/models/territory.go
@@ -1,21 +1,21 @@
 package models
 
 type Territory struct {
-    ID        int      `json:"id"`
-    UserID    int      `json:"user_id"`
-    Name      string   `json:"name"`
-    Latitude  float64  `json:"latitude"`
-    Longitude float64  `json:"longitude"`
-    Radius    float64  `json:"radius"`
-    MinAltitude float64 `json:"min_altitude"`
-    MaxAltitude float64 `json:"max_altitude"`
+    ID              int      `json:"id"`
+    UserID          int      `json:"user_id"`
+    Name            string   `json:"name"`
+    Latitude        float64  `json:"latitude"`
+    Longitude       float64  `json:"longitude"`
+    Radius          float64  `json:"radius"`
+    MinAltitude     float64 `json:"min_altitude"`
+    MaxAltitude     float64 `json:"max_altitude"`
 }
 
 type TerritoryRequest struct {
-    Name      string  `json:"name"`
-    Latitude  float64 `json:"latitude"`
-    Longitude float64 `json:"longitude"`
-    Radius    float64 `json:"radius"`
-    MinAltitude float64 `json:"min_altitude"`
-    MaxAltitude float64 `json:"max_altitude"`
+    Name            string  `json:"name"`
+    Latitude        float64 `json:"latitude"`
+    Longitude       float64 `json:"longitude"`
+    Radius          float64 `json:"radius"`
+    MinAltitude     float64 `json:"min_altitude"`
+    MaxAltitude     float64 `json:"max_altitude"`
 }
